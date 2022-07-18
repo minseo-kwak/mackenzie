40600a6fd63d41be17e3f962e3e12c66c0fe267c
Minseo <passxy@naver.com>
2022-07-18 18:25:40 +0900

    필터 기능 추가

--- js/order.js
+++ js/order.js
@@ -1,6 +1,32 @@
 $(function(){
     //메뉴 탭하여 원하는 섹션 보기
-    $()
+    $('#menu-filter ul li').click(function(e){
+        e.preventDefault();
+        var activeTab = $(this).attr('data-tab');
+        $('#menu-filter ul li').removeClass('active');
+        $('.tabcont').removeClass('active');
+        $(this).addClass('active');
+        $('#' + activeTab).addClass('active');
+
+        $('.detail-filter ul li:nth-child(1)').trigger('click').addClass('active');
+    });
+
+    //디테일 필터
+    var $grid = $('.order-menulist').isotope({
+    layoutMode: 'fitRows',
+    fitRows: {
+        gutter: 20
+      }
+    });
+
+    $('.detail-filter ul li').click(function(e){
+        e.preventDefault();
+        $('.detail-filter ul li').removeClass('active');
+        $(this).addClass('active');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+    
 
     //카트에 추가
     $('.order-menulist li .txt-box .btn-add').click(function(){
